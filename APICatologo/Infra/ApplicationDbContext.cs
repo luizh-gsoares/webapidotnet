fc6580413c0f2bca9d0e134d20aaec550a1e73c3
Luiz Henrique Soares <luizhenriquesoares18@hotmail.com>
2023-10-23 18:58:56 +0000

    Updating project files

---
+++ APICatologo/Infra/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Infra;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+
+}
