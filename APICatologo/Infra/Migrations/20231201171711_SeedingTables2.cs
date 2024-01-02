7f00fe8f968400cdc4c45f8d52f2c87b9ed4aa2d
Luiz Henrique Soares <luiz.henrique@confea.org.br>
2024-01-02 15:04:13 -0300

    Adding EF Migrations

---
+++ APICatologo/Infra/Migrations/20231201171711_SeedingTables2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingTables2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                   "Values('Coca-Cola Diet','Refrigerante de Cola 350 ml',5.45,'cocacola.jpg',50,GETUTCDATE(),1)");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                   "Values('Lanche de Atum','Lanche de Atum com maionese',8.50,'atum.jpg',10,GETUTCDATE(),2)");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                   "Values('Pudim 100 g','Pudim de leite condensado 100g',6.75,'pudim.jpg',20,GETUTCDATE(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
